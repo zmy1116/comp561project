11f188f0790cda8f79724e2eb4464bf8691502b0
myz1123 <mingyangzhou@yahoo.com>
2021-11-13 17:16:03 -0500

    Modify the query generation function to generate a set of queries  for 1 random label position or for all possible label positions

--- src/utils/generate_query.py
+++ src/utils/generate_query.py
@@ -2,28 +2,53 @@ import pickle
 import numpy as np
 
 
-def generate_vanilla_query(reference_matrix, query_length, characters="ACGT"):
+def generate_queries(reference_matrix, query_length, type='all_positions', num=1, with_substitution=False,
+                     characters="ACGT"):
     """
-    start from random position in ref sequence and generate query seq according
-    to ref seq probabilities
-    :param reference_matrix_file: matrix storing the proba of each letter at each
+    generate testing sequences according to ref seq probabilities
+    :param reference_matrix: matrix storing the proba of each letter at each
     position in ref sequence
-    :param length: desired length of the query sequence
+    :param query_length: desired length of the query sequence
+    :param type: generation type (all_positions: for every position generate queries, random: choose a random position)
+    :param num: number of queries generated for each position
+    :param with_substitution: whether perform extra substitutions
     :param characters: ordered list of characters, same order as in reference matrix
     :output: a query sequence generated according to the probabilities of the
     reference sequence, no extra substitution or indel yet
     """
-    # reference_matrix = pickle.load(open(reference_matrix_file, 'rb'))
+    assert type in ['all_positions', 'random']
+
     ref_length, nb_chars = reference_matrix.shape
-    query_pos = np.random.choice(np.arange(0, ref_length - query_length))
-    print(query_pos)
-    query = ""
-    for k in range(query_length):
-        probas = reference_matrix[query_pos]
-        char_index = np.random.choice(np.arange(nb_chars), p=probas)
-        query += characters[char_index]
-        query_pos += 1
-    return query
+    # define the choices of query positions
+    query_position_ranges = np.arange(0, ref_length - query_length)
+
+    # select list of labels: all positions or just a random position
+    if type == 'all_position':
+        # iterate through all positions
+        query_positions = query_position_ranges
+    else:
+        # choose a random position
+        query_positions = [np.random.choice(query_position_ranges)]
+
+    # for each query position, generate #num queries
+    queries_data = {}
+    for label in query_positions:
+        queries = []
+        for _ in range(num):
+            query = ''
+            for k in range(query_length):
+                probas = reference_matrix[k + label]
+                char_index = np.random.choice(np.arange(nb_chars), p=probas)
+                query += characters[char_index]
+
+            # TODO ADD SUBSTITUTION AND GAP
+            # if with_substitution:
+            #     query = add_substitutions(query, substitutions_probas=0, non_substitution_proba=0.9,
+            #                               characters=characters)
+            queries.append(query)
+        queries_data[label] = queries
+
+    return queries_data
 
 
 def add_substitutions(query, substitutions_probas=0, non_substitution_proba=0.9, characters="ACGT"):
@@ -47,9 +72,10 @@ def add_substitutions(query, substitutions_probas=0, non_substitution_proba=0.9,
         substitutions_probas = dict()
         for c1 in characters:
             for c2 in characters:
-                if c2 == c1: substitutions_probas[(c1, c2)]=non_substitution_proba
+                if c2 == c1:
+                    substitutions_probas[(c1, c2)] = non_substitution_proba
                 else:
-                    substitutions_probas[(c1, c2)] = (1-non_substitution_proba) / (len(characters)-1)
+                    substitutions_probas[(c1, c2)] = (1 - non_substitution_proba) / (len(characters) - 1)
     new_query = ""
     for i, c in enumerate(query):
         probas = [substitutions_probas[(c, c2)] for c2 in characters]
@@ -57,10 +83,3 @@ def add_substitutions(query, substitutions_probas=0, non_substitution_proba=0.9,
         new_query = new_query + characters[char_index]
 
     return new_query
-
-
-
-
-
-
-
