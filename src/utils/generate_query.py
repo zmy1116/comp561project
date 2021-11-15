5b38767a125f1cc77122dba076b168a6ea2c90f8
jea312 <jeanne.tous@gmail.com>
2021-11-15 12:15:47 -0500

    Added indel to query generation

--- src/utils/generate_query.py
+++ src/utils/generate_query.py
@@ -1,9 +1,12 @@
 import pickle
 import numpy as np
 
+# %%
 
 def generate_queries(reference_matrix, query_length, query_positions, num=1,
-                     with_substitution=False, with_indel=False, characters="ACGT"):
+                     with_substitution=False, with_indel=False,
+                     characters="ACGT", in_open=0.0017, in_extend=0.7,
+                     del_open=0.0017, del_extend=0.7, nt_distribution=0):
     """
     generate testing sequences according to ref seq probabilities
     :param reference_matrix: matrix storing the proba of each letter at each
@@ -13,6 +16,13 @@ def generate_queries(reference_matrix, query_length, query_positions, num=1,
     :param num: number of queries generated for each position
     :param with_substitution: whether perform extra substitutions
     :param characters: ordered list of characters, same order as in reference matrix
+    :param in_open: probability that an insertion starts
+    :param in_extend: probability that an insertion is extended
+    :param del_open: probability that an insertion starts
+    :param del_extend: probability that an insertion is extended
+    :nt_distribution: either 0 if non specified or a*list*, reference
+    nucleotide distribution to fill the insertions in an appropriate manner,
+    same order as in characters
     :output: list of dictionary {label position, query string}, no extra substitution or indel yet
     """
     ref_length, nb_chars = reference_matrix.shape
@@ -37,15 +47,15 @@ def generate_queries(reference_matrix, query_length, query_positions, num=1,
                 char_index = np.random.choice(np.arange(nb_chars), p=probas)
                 query += characters[char_index]
 
+            s=query
+
             if with_substitution:
                 query = add_substitutions(query, substitutions_probas=substitutions_probas,
                 non_substitution_proba=0.9, characters=characters)
-            print(s1)
-            print(s1==query)
-            # TODO ADD INDEL
-            # if with_indel:
-                # query = add_indel()
 
+            if with_indel:
+                query = add_indels(query, in_open, in_extend, del_open,
+                                   del_extend, characters, nt_distribution)
             queries_data.append(
                 {
                     'pos': label,
@@ -73,7 +83,7 @@ for nt1 in nucleotides:
 
 
 
-# %%
+# %% Adding substitutions
 def add_substitutions(query, substitutions_probas=substitutions_probas, non_substitution_proba=0.9, characters="ACGT"):
     """
     Starting from a query sequence, add substitutions to it
@@ -86,7 +96,6 @@ def add_substitutions(query, substitutions_probas=substitutions_probas, non_subs
     having a substitution
     :param characters: ordered list of characters, same order as in reference matrix
     :output: modified query with random substitutions in it
-
     """
     nb_chars = len(characters)
 
@@ -100,9 +109,76 @@ def add_substitutions(query, substitutions_probas=substitutions_probas, non_subs
                 else:
                     substitutions_probas[(c1, c2)] = (1 - non_substitution_proba) / (len(characters) - 1)
     new_query = ""
+
     for i, c in enumerate(query):
         probas = [substitutions_probas[(c, c2)] for c2 in characters]
         char_index = np.random.choice(np.arange(nb_chars), p=probas)
         new_query = new_query + characters[char_index]
 
+    return new_query
+
+
+# %% Adding indel(s)
+def add_indels(query, in_open=0.0017, in_extend=0.7, del_open=0.0017,
+               del_extend=0.7, characters="ACGT", nt_distribution=0):
+    """
+    Starting from a query sequence, add indels to it
+    :param query: query sequence to modify
+    :param in_open: probability that an insertion starts
+    :param in_extend: probability that an insertion is extended
+    :param del_open: probability that an insertion starts
+    :param del_extend: probability that an insertion is extended
+    :nt_distribution: either 0 if non specified or a*list*, reference
+    nucleotide distribution to fill the insertions in an appropriate manner
+    :param characters: ordered list of characters,same order as in
+    nt_distribution
+    """
+
+    if nt_distribution == 0:
+        nt_distribution = [1/(len(characters)) for nt in characters]
+
+    nb_chars = len(characters)
+
+    new_query = ""
+    current_pos = 0
+    ongoing_in = False
+    ongoing_del = False
+    probas_open = [in_open, del_open, 1-in_open-del_open]
+    probas_in = [in_extend, 1-in_extend]
+    probas_del = [del_extend, 1-del_extend]
+
+
+    while current_pos < len(query):
+        if ongoing_del:
+            extend = np.random.choice(np.arange(2), p=probas_del)
+            if extend == 0: current_pos += 1
+            else:
+                new_query += query[current_pos]
+                current_pos += 1
+                ongoing_del = False
+
+        elif ongoing_in:
+            extend = np.random.choice(np.arange(2), p=probas_in)
+            if extend == 0:
+                char_index = np.random.choice(np.arange(nb_chars), p=nt_distribution)
+                new_query += characters[char_index]
+            else:
+                new_query += query[current_pos]
+                current_pos += 1
+                ongoing_in = False
+
+        else:
+            event = np.random.choice(np.arange(3), p=probas_open)
+            if event == 2:
+                new_query += query[current_pos]
+                current_pos += 1
+            elif event == 0:
+                char_index = np.random.choice(np.arange(nb_chars), p=nt_distribution)
+                new_query += characters[char_index]
+                ongoing_in = True
+            else:
+                current_pos += 1
+                ongoing_del = True
+
+    res.append(query==new_query)
     return new_query
