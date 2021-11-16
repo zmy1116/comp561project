0c8d5a6876a2c9cae10d54f126c43ac5f67a6b35
jea312 <jeanne.tous@gmail.com>
2021-11-16 12:31:24 -0500

    Add generate_sequence function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -35,3 +35,27 @@ def seq2num(seq, letters='ACGT'):
     seq = [letter_map[x] for x in seq]
     seq = np.array(seq)
     return seq
+
+
+def generate_sequence(possible_letters_dict, fixed_length, saved_strings,
+                      current_string, characters="ACGT"):
+    """
+    Recursively find all possible strings of fixed length with letter at rank
+    k taken among letters in possible_letters_dict[k]
+    :param possible_letters_dict: possible letters for each position
+    :fixed_length: length of strings to generate
+    :param saved_strings: set of strings already computed
+    :param current_string: string being completed
+    :param characters: characters to use, order is the same as indices in
+    possible_letters_dict
+    :return: nothing but fills saved_strings
+    """
+    if len(current_string) == fixed_length: saved_strings.add(current_string)
+    else:
+        pos = len(current_string)
+        possible_letters = possible_letters_dict[pos]
+        for idx in possible_letters:
+            new_current_string = current_string + characters[idx]
+            generate_sequence(possible_letters_dict, fixed_length, saved_strings,
+                              new_current_string, characters="ACGT")
+
