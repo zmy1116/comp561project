49fe84e4a270d626274d620bc2c84d1133578e01
myz1123 <mingyangzhou@yahoo.com>
2021-11-27 23:03:26 -0500

    add arg parse  in complete_blast
    add readme

--- README.md
+++ README.md
@@ -26,3 +26,10 @@ python /home/mcb/users/mzhou3/comp561project/src/preprocessing.py \
 	--output_path=/home/mcb/users/mzhou3/comp561project/src/data/reference_matrix.p
 ```
 
+- `complete_blast.py` do full blast 
+```
+python /home/mcb/users/mzhou3/comp561project/src/blast/complete_blast.py \
+    --output_file=test \
+    --reference_matrix_file=/home/NOBACKUP/mzhou3/comp561project/src/data/reference_matrix.p \
+    --query_file=/home/mcb/users/mzhou3/comp561project/src/data/example_query.fa
+```
--- src/blast/complete_blast.py
+++ src/blast/complete_blast.py
@@ -1,10 +1,12 @@
-import numpy as np
+import os
 import pickle
 import json
-from src.utils.generate_query import generate_queries
+import argparse
 from src.blast.hashtable_generation_inference import consensus_seq_match
 from src.blast.ungapped_extension import ungapped_extension
 from src.blast.gapped_extension_j_mingmodified import gapped_extension
+
+DEFAULT_BLAST_ARGS = json.load(open(os.path.join(os.path.dirname(__file__), 'config', 'default_blast_args.json'), 'r'))
 
 
 # %%
@@ -39,3 +41,77 @@ def blast(query, reference_matrix_file, seed_matching_args, ungapped_extension_a
     outputs_step3 = sorted(outputs_step3, key=lambda x: x['final_result']['score'] * -1)
 
     return outputs_step3
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output_file",
+        type=str,
+        help="output storage path"
+    )
+
+    parser.add_argument(
+        "--reference_matrix_file",
+        type=str,
+        help="file path of the reference matrix file"
+    )
+
+    parser.add_argument(
+        "--query_file",
+        type=str,
+        help="file path of the query, if None use a randomly generate query",
+        default=None
+    )
+
+    parser.add_argument(
+        "--config_file",
+        type=str,
+        help="file path of the algorithm configs, if None, use the default blast arguments file",
+        default=None)
+    parser.add_argument(
+        "--hashtable_data_file",
+        type=str,
+        help="file path of the hashtable file, if None, use the default path",
+        default=None
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    if args.query_file is not None:
+        query = open(args.query_file).readlines()[0]
+    else:
+        # use an example query
+        query = 'TAAACCCCGGGCCATATTATGCTGGGAGGCTCACCTCTGTAATGTCGGGGTCTTGGAGGCTGAGGAGGTATTGCTTTGGGGAGTTCAAGCCCAGCCTGGG'
+
+    if args.config_file is not None:
+        blast_configs = json.load(open(args.config_file, 'r'))
+    else:
+        # use default args
+        blast_configs = DEFAULT_BLAST_ARGS
+
+    if args.hashtable_data_file is not None:
+        args['seed_matching_args']['seed_table_file'] = args.hashtable_data_file
+
+    reference_matrix_file = args.reference_matrix_file
+
+    outputs = blast(query, reference_matrix_file, **blast_configs)
+    for idx, out in enumerate(outputs):
+        print('{0}  ref_start_idx:{1},   ref_end_idx:{2},    score: {3}'.format(idx,
+                                                                                out['final_result']['ref_left_idx'],
+                                                                                out['final_result']['ref_right_idx'],
+                                                                                out['final_result']['score']))
+    result = {
+        'query': query,
+        'result': outputs
+    }
+    output_file = args.output_file
+    pickle.dump(result, open(output_file, 'wb'))
+
+
+if __name__ == '__main__':
+    main()
